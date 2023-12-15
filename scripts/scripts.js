7015fee8512d6f58c2806e939acbc14f7467137c
AdamCanard <azcunard@gmail.com>
2023-12-14 22:01:49 -0700

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,69 @@
+const searchBar = document.querySelector(".searchBar");
+const semesterBox = document.querySelector(".semesterBox");
+<<<<<<< HEAD
+const api = null; // root link for API calls
+const breadcrumb = document.querySelector(".breadcrumb");
+=======
+const api = null // root link for API calls
+const breadcrumb = document.querySelector(".breadcrumb")
+const nav = document.querySelector("nav")
+>>>>>>> 0b61fcddc87079becd1ffff078cdb64445dbe38b
+
+//searchBar.addEventListener("input", search());
+const nav = document.getElementById("n");
+
+function search() {
+  // Takes user input, calls database API for each character entered after 1 second of no typing (for server efficiency)
+  // Search bar appears on every page. Search function will be called by eventlistener listening for characters entered in input
+  // displays search results as list of links popped up below the search bar
+  displaySearch();
+}
+
+function displaySearch() {
+  // dynamically displays results obtained from search() function as a list of links below search bar
+}
+
+function assignProgramLength() {
+  // for program in programList:
+  //
+}
+
+// for (elem in softwareDev) {
+//   newElem = document.createElement("div");
+//   newElem.innerHtml = elem;
+//   document.getElementById("n").appendChild(newElem);
+// }
+
+function navigation() {
+  // for each program in program list:
+  //      append program to sidebar nav with correct href
+  const programList = [
+    "Software Development",
+    "Web Development",
+    "Network Systems",
+  ];
+  for (let program of programList) {
+    console.log(program);
+    newElem = document.createElement("p");
+    newElem.innerHTML = program;
+    nav.append(newElem);
+  }
+}
+
+function updateBreadCrumb() {
+  // Page name, program name, and semester number are stored in variables
+  // Updates breadcrumb div with proper information
+}
+
+function parseProgram() {
+    
+}
+
+function populateBox() {
+  // Calls API for specific page, program, and semester
+  // For each semester div/block:
+  //      append semester number
+  //      append courses with anchor links to course page
+}
+
+navigation();
