9b62a3150c9f690c6f4e2da6f66e027e1fdd93b0
AdamCanard <azcunard@gmail.com>
2023-12-15 15:46:37 -0700

    finished?

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,31 +11,35 @@ let extractedCourses = [];
 // contains programs and number of semesters as key-val pairs
 let numSemesters = {};
 
-searchSubmit.addEventListener("click", search());
+searchSubmit.addEventListener("click", fetchCourseJson);
 searchBar.addEventListener("input", search);
 
-fetchProgramJson();
+async function fetchCourseJson(input) {
+  console.log(input);
+  let temp = await fetch("http://server.jgaribsin.com:3000/courses/findAll", {
+    headers: {},
+    method: "GET",
+  });
+  return temp.json();
+}
 
-function search() {
+async function search() {
   //console.log(extractedCourses);
   // Takes user input, searches through course list parsed through json for each character entered
   // Search bar appears on every page. Search function will be called by eventlistener listening for characters entered in input
   // displays search results as list of links popped up below the search bar
-  while (searchOutput.firstChild) {
-    searchOutput.removeChild(searchOutput.firstChild);
-  }
+  data = await fetchCourseJson();
+  let courses = data.data.courses;
+  console.log(courses);
 
-  for (let i = 0; i < extractedCourses.length; i++) {
-    if (
-      extractedCourses[i].toLowerCase().includes(searchBar.value) &&
-      searchBar.value
-    ) {
+  for (let i = 0; i < courses.length; i++) {
+    if (courses[i].toLowerCase().includes(searchBar.value) && searchBar.value) {
       newElem = document.createElement("a");
       newElem.href =
-        "/pages/course.html?code=" + encodeURIComponent(extractedCourses[i]);
+        "/pages/course.html?code=" + encodeURIComponent(courses[i]);
       newElem.classList.add("course-anchor");
       newElem.onclick = loadCoursePage;
-      newElem.textContent = extractedCourses[i];
+      newElem.textContent = courses[i];
       searchOutput.append(newElem);
     }
   }
@@ -66,20 +70,6 @@ function assignProgramData(data) {
   }
   //console.log(extractedCourses);
   return extractedCourses;
-}
-
-function fetchProgramJson() {
-  fetch("./scripts/software_development.json")
-    //fetch("http://server.jgaribsin.com:3000/courses/find", {
-    //method:'GET',
-    //credentials:'same-origin'})
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      //console.log(data);
-      assignProgramData(data);
-    });
 }
 
 function navigation() {
