559ce95ea8640182ec36386b7f565f0119fa8e34
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-15 08:27:56 -0700

    styled search to show up in column

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -40,7 +40,7 @@ fetchProgramJson();
 
 function search() {
   //console.log(extractedCourses);
-  // Takes user input, calls database API for each character entered after 1 second of no typing (for server efficiency)
+  // Takes user input, searches through course list parsed through json for each character entered
   // Search bar appears on every page. Search function will be called by eventlistener listening for characters entered in input
   // displays search results as list of links popped up below the search bar
   while (searchOutput.firstChild) {
@@ -49,7 +49,9 @@ function search() {
 
   for (let i = 0; i < extractedCourses.length; i++) {
     if (extractedCourses[i].toLowerCase().includes(searchBar.value) && searchBar.value) {
-      newElem = document.createElement('p');
+      newElem = document.createElement('a');
+      newElem.href = "/pages/course.html";
+      newElem.classList.add("course-anchor");
       newElem.textContent = extractedCourses[i];
       searchOutput.append(newElem);
     }
@@ -89,7 +91,6 @@ function fetchProgramJson() {
       return res.json();
     })
     .then((data) => {
-      program = data;
       //console.log(data);
       assignProgramData(data);
     });
