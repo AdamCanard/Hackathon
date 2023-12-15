0d05efaba7974e8ae33fb63dbdf2238b77332afd
jgaribsin <jonathan.garibsingh@gmail.com>
2023-12-15 13:29:30 -0700

    adding `cors` to allow access from http and https

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import path from 'path';
 import router from './routes';
+import cors from 'cors';
 
 const PORT = 3000;
 const VERSION = '0.0.0';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
